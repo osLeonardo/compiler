08200ba9f97ddf0c4f2953aeb53ec2d14d1a5db8
Leonardo Spilere <leonardo.spilere@deps.com.br>
2024-11-22 15:45:40 -0300

    eu não aguento mais

--- service/simbolos.py
+++ service/simbolos.py
@@ -4,13 +4,13 @@ class TabelaSimbolos:
 
     def insert(self, name, category, type, level):
         if name in self.table:
-            return f"Erro: variável '{name}' já foi declarada."
+            return f"Erro: identificador '{name}' já foi declarado como {self.table[name]['category']}."
         self.table[name] = {'category': category, 'type': type, 'level': level}
         return "OK"
 
     def lookup(self, name):
         if name not in self.table:
-            return f"Erro: variável '{name}' não foi declarada."
+            return f"Erro: identificador '{name}' não foi declarado."
         return self.table[name]
 
     def remove(self, name):
@@ -19,7 +19,7 @@ class TabelaSimbolos:
 
     def check_scope(self, name, current_level):
         if name not in self.table or self.table[name]['level'] != current_level:
-            return f"Erro: variável '{name}' fora do escopo."
+            return f"Erro: identificador '{name}' fora do escopo."
         return "OK"
 
     def get_all_symbols(self):
